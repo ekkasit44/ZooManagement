0883b1663cd112e2958d6cee87633ad127422022
Naruesorn wichit 115 <Nakoss4399@gmail.com>
2026-03-07 18:12:58 +0700

    Add files via upload

---
+++ ZooManagement/MDIForm.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace ZooManagement
+{
+    partial class MDIForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            mnuF = new ToolStripMenuItem();
+            mnuMax = new ToolStripMenuItem();
+            toolStripMenuItem1 = new ToolStripSeparator();
+            mnuMin = new ToolStripMenuItem();
+            toolStripMenuItem2 = new ToolStripSeparator();
+            mnuExit = new ToolStripMenuItem();
+            toolStripMenuItem3 = new ToolStripSeparator();
+            mnuNomalF = new ToolStripMenuItem();
+            mnuCloseF = new ToolStripMenuItem();
+            mnuCloseAllF = new ToolStripMenuItem();
+            mnuAnimalform = new ToolStripMenuItem();
+            mnuAnimal = new ToolStripMenuItem();
+            mnuFood = new ToolStripMenuItem();
+            mnuAnimalType = new ToolStripMenuItem();
+            mnuSpeciesInfo = new ToolStripMenuItem();
+            mnuEfrm = new ToolStripMenuItem();
+            nmuEnclosure = new ToolStripMenuItem();
+            nmuKeeper = new ToolStripMenuItem();
+            mnuArrangeF = new ToolStripMenuItem();
+            mnuVertical = new ToolStripMenuItem();
+            mnuHorizontal = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { mnuF, mnuAnimalform, mnuEfrm, mnuArrangeF });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(419, 28);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "menuStrip1";
+            menuStrip1.ItemClicked += menuStrip1_ItemClicked;
+            // 
+            // mnuF
+            // 
+            mnuF.BackColor = Color.Blue;
+            mnuF.DropDownItems.AddRange(new ToolStripItem[] { mnuMax, toolStripMenuItem1, mnuMin, toolStripMenuItem2, mnuExit, toolStripMenuItem3, mnuNomalF, mnuCloseF, mnuCloseAllF });
+            mnuF.Name = "mnuF";
+            mnuF.Size = new Size(55, 24);
+            mnuF.Text = "ฟอร์ม";
+            // 
+            // mnuMax
+            // 
+            mnuMax.Name = "mnuMax";
+            mnuMax.Size = new Size(183, 26);
+            mnuMax.Text = "ขยาย ";
+            mnuMax.Click += mnuMax_Click;
+            // 
+            // toolStripMenuItem1
+            // 
+            toolStripMenuItem1.Name = "toolStripMenuItem1";
+            toolStripMenuItem1.Size = new Size(180, 6);
+            // 
+            // mnuMin
+            // 
+            mnuMin.Name = "mnuMin";
+            mnuMin.Size = new Size(183, 26);
+            mnuMin.Text = "ย่อ  ";
+            mnuMin.Click += mnuMin_Click;
+            // 
+            // toolStripMenuItem2
+            // 
+            toolStripMenuItem2.Name = "toolStripMenuItem2";
+            toolStripMenuItem2.Size = new Size(180, 6);
+            // 
+            // mnuExit
+            // 
+            mnuExit.Name = "mnuExit";
+            mnuExit.Size = new Size(183, 26);
+            mnuExit.Text = "ปิดโปรแกรม ";
+            mnuExit.Click += mnuExit_Click;
+            // 
+            // toolStripMenuItem3
+            // 
+            toolStripMenuItem3.Name = "toolStripMenuItem3";
+            toolStripMenuItem3.Size = new Size(180, 6);
+            // 
+            // mnuNomalF
+            // 
+            mnuNomalF.Name = "mnuNomalF";
+            mnuNomalF.Size = new Size(183, 26);
+            mnuNomalF.Text = "ปกติ";
+            mnuNomalF.Click += mnuNomalF_Click;
+            // 
+            // mnuCloseF
+            // 
+            mnuCloseF.Name = "mnuCloseF";
+            mnuCloseF.Size = new Size(183, 26);
+            mnuCloseF.Text = "ปิดฟอร์ม";
+            mnuCloseF.Click += mnuCloseF_Click;
+            // 
+            // mnuCloseAllF
+            // 
+            mnuCloseAllF.Name = "mnuCloseAllF";
+            mnuCloseAllF.Size = new Size(183, 26);
+            mnuCloseAllF.Text = "ปิดฟอร์มทั้งหมด";
+            mnuCloseAllF.Click += mnuCloseAllF_Click;
+            // 
+            // mnuAnimalform
+            // 
+            mnuAnimalform.BackColor = Color.FromArgb(128, 255, 128);
+            mnuAnimalform.DropDownItems.AddRange(new ToolStripItem[] { mnuAnimal, mnuFood, mnuAnimalType, mnuSpeciesInfo });
+            mnuAnimalform.Name = "mnuAnimalform";
+            mnuAnimalform.Size = new Size(119, 24);
+            mnuAnimalform.Text = "จัดการข้อมูลสัตว์";
+            mnuAnimalform.Click += mnuAnimalform_Click;
+            // 
+            // mnuAnimal
+            // 
+            mnuAnimal.Name = "mnuAnimal";
+            mnuAnimal.Size = new Size(224, 26);
+            mnuAnimal.Text = "ข้อมูลสัตว์";
+            mnuAnimal.Click += mnuAnimal_Click;
+            // 
+            // mnuFood
+            // 
+            mnuFood.Name = "mnuFood";
+            mnuFood.Size = new Size(224, 26);
+            mnuFood.Text = "ข้อมูลอาหาร";
+            mnuFood.Click += mnuFood_Click;
+            // 
+            // mnuAnimalType
+            // 
+            mnuAnimalType.Name = "mnuAnimalType";
+            mnuAnimalType.Size = new Size(224, 26);
+            mnuAnimalType.Text = "ข้อมูลประเภทของสัตว์";
+            mnuAnimalType.Click += mnuAnimalType_Click;
+            // 
+            // mnuSpeciesInfo
+            // 
+            mnuSpeciesInfo.Name = "mnuSpeciesInfo";
+            mnuSpeciesInfo.Size = new Size(224, 26);
+            mnuSpeciesInfo.Text = "ข้อมูลชนิดพันธุ์";
+            mnuSpeciesInfo.Click += mnuSpeciesInfo_Click;
+            // 
+            // mnuEfrm
+            // 
+            mnuEfrm.BackColor = Color.FromArgb(255, 255, 128);
+            mnuEfrm.DropDownItems.AddRange(new ToolStripItem[] { nmuEnclosure, nmuKeeper });
+            mnuEfrm.Name = "mnuEfrm";
+            mnuEfrm.Size = new Size(140, 24);
+            mnuEfrm.Text = "จัดการข้อมูลกรงสัตว์";
+            mnuEfrm.Click += mnuEfrm_Click;
+            // 
+            // nmuEnclosure
+            // 
+            nmuEnclosure.Name = "nmuEnclosure";
+            nmuEnclosure.Size = new Size(203, 26);
+            nmuEnclosure.Text = "ข้อมูลกรงสัตว์";
+            nmuEnclosure.Click += mnuEnclosure_Click;
+            // 
+            // nmuKeeper
+            // 
+            nmuKeeper.Name = "nmuKeeper";
+            nmuKeeper.Size = new Size(203, 26);
+            nmuKeeper.Text = "ข้อมูลผู้ดูแลกรงสัตว์";
+            nmuKeeper.Click += mnuKeeper_Click;
+            // 
+            // mnuArrangeF
+            // 
+            mnuArrangeF.BackColor = Color.Blue;
+            mnuArrangeF.DropDownItems.AddRange(new ToolStripItem[] { mnuVertical, mnuHorizontal });
+            mnuArrangeF.Name = "mnuArrangeF";
+            mnuArrangeF.Size = new Size(96, 24);
+            mnuArrangeF.Text = "จัดเรียงฟอร์ม";
+            // 
+            // mnuVertical
+            // 
+            mnuVertical.Name = "mnuVertical";
+            mnuVertical.Size = new Size(141, 26);
+            mnuVertical.Text = "แนวตั้ง";
+            mnuVertical.Click += mnuVertical_Click;
+            // 
+            // mnuHorizontal
+            // 
+            mnuHorizontal.Name = "mnuHorizontal";
+            mnuHorizontal.Size = new Size(141, 26);
+            mnuHorizontal.Text = "แนวนอน";
+            mnuHorizontal.Click += mnuHorizontal_Click;
+            // 
+            // MDIForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(419, 448);
+            Controls.Add(menuStrip1);
+            IsMdiContainer = true;
+            MainMenuStrip = menuStrip1;
+            Name = "MDIForm";
+            Text = "ZooManagement";
+            Load += MDIForm_Load;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem mnuF;
+        private ToolStripMenuItem mnuMax;
+        private ToolStripMenuItem mnuMin;
+        private ToolStripMenuItem mnuExit;
+        private ToolStripSeparator toolStripMenuItem1;
+        private ToolStripSeparator toolStripMenuItem2;
+        private ToolStripSeparator toolStripMenuItem3;
+        private ToolStripMenuItem mnuArrangeF;
+        private ToolStripMenuItem mnuVertical;
+        private ToolStripMenuItem mnuHorizontal;
+        private ToolStripMenuItem mnuNomalF;
+        private ToolStripMenuItem mnuCloseF;
+        private ToolStripMenuItem mnuCloseAllF;
+        private ToolStripMenuItem mnuAnimalform;
+        private ToolStripMenuItem mnuAnimal;
+        private ToolStripMenuItem mnuAnimalType;
+        private ToolStripMenuItem mnuSpeciesInfo;
+        private ToolStripMenuItem mnuEfrm;
+        private ToolStripMenuItem nmuEnclosure;
+        private ToolStripMenuItem nmuKeeper;
+        private ToolStripMenuItem mnuFood;
+    }
+}
---
+++ ZooManagement/MDIForm.cs
@@ -0,0 +1,169 @@
+﻿namespace ZooManagement
+{
+    public partial class MDIForm : Form
+    {
+        public MDIForm()
+        {
+            InitializeComponent();
+        }
+        private void mnuMax_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void mnuMin_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+
+
+
+
+
+
+        private void mnuExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void mnuVertical_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.TileVertical);
+        }
+
+        private void mnuHorizontal_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.TileHorizontal);
+        }
+
+        private void mnuNomalF_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void mnuCloseF_Click(object sender, EventArgs e)
+        {
+            if (this.MdiChildren.Length > 0)
+            {
+
+                this.MdiChildren[0].Close();
+            }
+        }
+
+        private void mnuCloseAllF_Click(object sender, EventArgs e)
+        {
+            foreach (Form child in this.MdiChildren)
+            {
+                child.Close();
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+        private void mnuAnimalform_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void mnuAnimalType_Click(object sender, EventArgs e)
+        {
+            AnimalTypeForm fE = new AnimalTypeForm();
+            fE.MdiParent = this;
+            fE.Show();
+        }
+
+        private void mnuSpeciesInfo_Click(object sender, EventArgs e)
+        {
+            SpeciesInfoForm FC = new SpeciesInfoForm();
+            FC.MdiParent = this;
+            FC.Show();
+        }
+
+        private void mnuEnclosure_Click(object sender, EventArgs e)
+        {
+            EnclosureForm fPC = new EnclosureForm();
+            fPC.MdiParent = this;
+            fPC.Show();
+        }
+
+        private void mnuKeeper_Click(object sender, EventArgs e)
+        {
+            KeeperForm fES = new KeeperForm();
+            fES.MdiParent = this;
+            fES.Show();
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            //ShowMenuManager();
+            //ShowMenuSale();
+            ShowMenuStart();
+        }
+
+
+
+
+
+
+
+
+        private void MDIForm_Load(object sender, EventArgs e)
+        {
+            ShowMenuStart();
+        }
+
+
+
+        private void mnuEfrm_Click(object sender, EventArgs e)
+        {
+
+        }
+
+      
+        private void mnuFood_Click(object sender, EventArgs e)
+        {
+            FoodForm fFood = new FoodForm();
+            fFood.MdiParent = this;
+            fFood.Show();
+        }
+
+       
+        private void ShowMenuStart()
+        {
+           
+            mnuF.Enabled = true;
+            mnuAnimalform.Enabled = true;
+            mnuEfrm.Enabled = true;
+            mnuArrangeF.Enabled = true;
+        }
+
+        private void mnuAnimal_Click(object sender, EventArgs e)
+        {
+            // เช็คว่ามีฟอร์ม AnimalForm เปิดอยู่หรือยัง
+            foreach (Form child in this.MdiChildren)
+            {
+                if (child is AnimalForm)
+                {
+                    child.Activate(); // ถ้ามีแล้วให้ดึงขึ้นมาหน้าสุด
+                    return;
+                }
+            }
+            
+            AnimalForm fP = new AnimalForm();
+            fP.MdiParent = this;
+            fP.Show();
+        }
+    }
+}
+
