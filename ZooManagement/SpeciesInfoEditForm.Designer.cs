913fea29991f89d35fa76ae09cc48a076ea1f6e8
Inthira4548 <kineqwasg@gmail.com>
2026-03-11 22:53:13 +0700

    Update SpeciesInfoEditForm.Designer.cs

--- ZooManagement/SpeciesInfoEditForm.Designer.cs
+++ ZooManagement/SpeciesInfoEditForm.Designer.cs
@@ -1,99 +1,108 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
+
 namespace ZooManagement
 {
-    partial class SpeciesInfoEditForm
+    public partial class SpeciesInfoEditForm : Form
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
-
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
+        public SpeciesInfoEditForm()
         {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
+            InitializeComponent();
         }
 
-        #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
+
+
+
+
+
+        private void btnAllAnimals_Click(object sender, EventArgs e)
         {
-            btnAllAnimals = new Button();
-            btnZoneAnimals = new Button();
-            btnFeed = new Button();
-            dataGridView1 = new DataGridView();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
-            SuspendLayout();
-            // 
-            // btnAllAnimals
-            // 
-            btnAllAnimals.Location = new Point(54, 59);
-            btnAllAnimals.Name = "btnAllAnimals";
-            btnAllAnimals.Size = new Size(205, 101);
-            btnAllAnimals.TabIndex = 0;
-            btnAllAnimals.Text = "รายงานสัตว์ทั้งหมด";
-            btnAllAnimals.UseVisualStyleBackColor = true;
-            btnAllAnimals.Click += btnAllAnimals_Click;
-            // 
-            // btnZoneAnimals
-            // 
-            btnZoneAnimals.Location = new Point(54, 209);
-            btnZoneAnimals.Name = "btnZoneAnimals";
-            btnZoneAnimals.Size = new Size(205, 101);
-            btnZoneAnimals.TabIndex = 1;
-            btnZoneAnimals.Text = "รายงานสัตว์แยกตามโซน";
-            btnZoneAnimals.UseVisualStyleBackColor = true;
-            btnZoneAnimals.Click += btnZoneAnimals_Click;
-            // 
-            // btnFeed
-            // 
-            btnFeed.Location = new Point(54, 366);
-            btnFeed.Name = "btnFeed";
-            btnFeed.Size = new Size(205, 101);
-            btnFeed.TabIndex = 2;
-            btnFeed.Text = "รายงานการให้อาหารรายวัน";
-            btnFeed.UseVisualStyleBackColor = true;
-            btnFeed.Click += btnFeed_Click;
-            // 
-            // dataGridView1
-            // 
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(336, 59);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.Size = new Size(871, 408);
-            dataGridView1.TabIndex = 3;
-            // 
-            // SpeciesInfoEditForm
-            // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1287, 702);
-            Controls.Add(dataGridView1);
-            Controls.Add(btnFeed);
-            Controls.Add(btnZoneAnimals);
-            Controls.Add(btnAllAnimals);
-            Name = "SpeciesInfoEditForm";
-            Text = "SpeciesInfoEditForm";
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
-            ResumeLayout(false);
+            SqlConnection conn = connectDB.ConnectZooDB();
+
+
+
+            string sql = @"SELECT 
+            a.name,
+            s.common_name,
+            e.name AS enclosure
+            FROM Animal a
+            JOIN SpeciesInfo s ON a.species_info_id = s.species_info_id
+            JOIN Enclosure e ON a.enclosure_id = e.enclosure_id";
+
+            SqlDataAdapter da = new SqlDataAdapter(sql, conn);
+
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            dataGridView1.DataSource = dt;
         }
 
-        #endregion
+        private void btnZoneAnimals_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = connectDB.ConnectZooDB();
 
-        private Button btnAllAnimals;
-        private Button btnZoneAnimals;
-        private Button btnFeed;
-        private DataGridView dataGridView1;
+            string sql = @"SELECT 
+            e.name,
+            COUNT(a.animal_id) AS total_animals
+            FROM Enclosure e
+            LEFT JOIN Animal a ON e.enclosure_id = a.enclosure_id
+            GROUP BY e.name";
+
+            SqlDataAdapter da = new SqlDataAdapter(sql, conn);
+
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            dataGridView1.DataSource = dt;
+        }
+
+        private void btnFeed_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = connectDB.ConnectZooDB();
+
+            string sql = @"SELECT 
+a.name AS AnimalName,
+f.name AS FoodName,
+fs.amount,
+fs.feeding_date,
+fs.feeding_time
+FROM FeedingSchedule fs
+JOIN Animal a ON fs.animal_id = a.animal_id
+JOIN Food f ON fs.food_id = f.food_id";
+
+            SqlDataAdapter da = new SqlDataAdapter(sql, conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            dataGridView1.DataSource = dt;
+        }
+
+        private void SpeciesInfoEditForm_Load(object sender, EventArgs e)
+        {
+            LoadAllAnimals();
+        }
+        void LoadAllAnimals()
+        {
+            SqlConnection conn = connectDB.ConnectZooDB();
+            string sql = @"SELECT 
+            a.name,
+            s.common_name,
+            e.name AS enclosure
+            FROM Animal a
+            JOIN SpeciesInfo s ON a.species_info_id = s.species_info_id
+            JOIN Enclosure e ON a.enclosure_id = e.enclosure_id";
+            SqlDataAdapter da = new SqlDataAdapter(sql, conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
     }
 }
