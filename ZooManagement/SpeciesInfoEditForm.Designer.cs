d0f018a72d09e4f61031ee75a145693033d1b5b7
Inthira4548 <kineqwasg@gmail.com>
2026-03-11 23:07:16 +0700

    Rename KeeperEditForm to SpeciesInfoEditForm

--- ZooManagement/SpeciesInfoEditForm.Designer.cs
+++ ZooManagement/SpeciesInfoEditForm.Designer.cs
@@ -1,6 +1,6 @@
 namespace ZooManagement
 {
-    partial class KeeperEditForm
+    partial class SpeciesInfoEditForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,203 +28,73 @@ namespace ZooManagement
         /// </summary>
         private void InitializeComponent()
         {
-            sqlCommand1 = new Microsoft.Data.SqlClient.SqlCommand();
-            sqlCommand2 = new Microsoft.Data.SqlClient.SqlCommand();
-            txtName = new TextBox();
-            txtPhone = new TextBox();
-            numericCapacity = new NumericUpDown();
-            comboPosition = new ComboBox();
+            btnAllAnimals = new Button();
+            btnZoneAnimals = new Button();
+            btnFeed = new Button();
             dataGridView1 = new DataGridView();
-            btnAdd = new Button();
-            btnDelete = new Button();
-            btnEdit = new Button();
-            btnOK = new Button();
-            txtEmail = new TextBox();
-            label1 = new Label();
-            label2 = new Label();
-            label3 = new Label();
-            label4 = new Label();
-            ((System.ComponentModel.ISupportInitialize)numericCapacity).BeginInit();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
-            // sqlCommand1
+            // btnAllAnimals
             // 
-            sqlCommand1.CommandTimeout = 30;
-            sqlCommand1.EnableOptimizedParameterBinding = false;
+            btnAllAnimals.Location = new Point(54, 59);
+            btnAllAnimals.Name = "btnAllAnimals";
+            btnAllAnimals.Size = new Size(205, 101);
+            btnAllAnimals.TabIndex = 0;
+            btnAllAnimals.Text = "รายงานสัตว์ทั้งหมด";
+            btnAllAnimals.UseVisualStyleBackColor = true;
+            btnAllAnimals.Click += btnAllAnimals_Click;
             // 
-            // sqlCommand2
+            // btnZoneAnimals
             // 
-            sqlCommand2.CommandTimeout = 30;
-            sqlCommand2.EnableOptimizedParameterBinding = false;
+            btnZoneAnimals.Location = new Point(54, 209);
+            btnZoneAnimals.Name = "btnZoneAnimals";
+            btnZoneAnimals.Size = new Size(205, 101);
+            btnZoneAnimals.TabIndex = 1;
+            btnZoneAnimals.Text = "รายงานสัตว์แยกตามโซน";
+            btnZoneAnimals.UseVisualStyleBackColor = true;
+            btnZoneAnimals.Click += btnZoneAnimals_Click;
             // 
-            // txtName
+            // btnFeed
             // 
-            txtName.Location = new Point(109, 55);
-            txtName.Name = "txtName";
-            txtName.Size = new Size(245, 27);
-            txtName.TabIndex = 0;
-            // 
-            // txtPhone
-            // 
-            txtPhone.Location = new Point(109, 132);
-            txtPhone.Name = "txtPhone";
-            txtPhone.Size = new Size(245, 27);
-            txtPhone.TabIndex = 1;
-            // 
-            // numericCapacity
-            // 
-            numericCapacity.Location = new Point(955, 55);
-            numericCapacity.Name = "numericCapacity";
-            numericCapacity.Size = new Size(211, 27);
-            numericCapacity.TabIndex = 2;
-            // 
-            // comboPosition
-            // 
-            comboPosition.FormattingEnabled = true;
-            comboPosition.Location = new Point(508, 55);
-            comboPosition.Name = "comboPosition";
-            comboPosition.Size = new Size(151, 28);
-            comboPosition.TabIndex = 3;
+            btnFeed.Location = new Point(54, 366);
+            btnFeed.Name = "btnFeed";
+            btnFeed.Size = new Size(205, 101);
+            btnFeed.TabIndex = 2;
+            btnFeed.Text = "รายงานการให้อาหารรายวัน";
+            btnFeed.UseVisualStyleBackColor = true;
+            btnFeed.Click += btnFeed_Click;
             // 
             // dataGridView1
             // 
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(63, 243);
+            dataGridView1.Location = new Point(336, 59);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.Size = new Size(773, 410);
-            dataGridView1.TabIndex = 4;
-            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            dataGridView1.Size = new Size(871, 408);
+            dataGridView1.TabIndex = 3;
             // 
-            // btnAdd
-            // 
-            btnAdd.Location = new Point(1081, 290);
-            btnAdd.Name = "btnAdd";
-            btnAdd.Size = new Size(94, 29);
-            btnAdd.TabIndex = 5;
-            btnAdd.Text = "เพิ่ม";
-            btnAdd.UseVisualStyleBackColor = true;
-            btnAdd.Click += btnAdd_Click;
-            // 
-            // btnDelete
-            // 
-            btnDelete.Location = new Point(1081, 370);
-            btnDelete.Name = "btnDelete";
-            btnDelete.Size = new Size(94, 29);
-            btnDelete.TabIndex = 6;
-            btnDelete.Text = "ลบ";
-            btnDelete.UseVisualStyleBackColor = true;
-            btnDelete.Click += btnDelete_Click;
-            // 
-            // btnEdit
-            // 
-            btnEdit.Location = new Point(1081, 461);
-            btnEdit.Name = "btnEdit";
-            btnEdit.Size = new Size(94, 29);
-            btnEdit.TabIndex = 7;
-            btnEdit.Text = "เเกไข้";
-            btnEdit.UseVisualStyleBackColor = true;
-            btnEdit.Click += btnEdit_Click;
-            // 
-            // btnOK
-            // 
-            btnOK.Location = new Point(1081, 547);
-            btnOK.Name = "btnOK";
-            btnOK.Size = new Size(94, 29);
-            btnOK.TabIndex = 8;
-            btnOK.Text = "ตกลง";
-            btnOK.UseVisualStyleBackColor = true;
-            btnOK.Click += btnOK_Click;
-            // 
-            // txtEmail
-            // 
-            txtEmail.Location = new Point(109, 188);
-            txtEmail.Name = "txtEmail";
-            txtEmail.Size = new Size(245, 27);
-            txtEmail.TabIndex = 9;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(24, 54);
-            label1.Name = "label1";
-            label1.Size = new Size(79, 20);
-            label1.TabIndex = 10;
-            label1.Text = "ชื่อเจ้าหน้าที่";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Location = new Point(18, 139);
-            label2.Name = "label2";
-            label2.Size = new Size(85, 20);
-            label2.TabIndex = 11;
-            label2.Text = "เบอร์โทรศัพท์";
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Location = new Point(34, 191);
-            label3.Name = "label3";
-            label3.Size = new Size(37, 20);
-            label3.TabIndex = 12;
-            label3.Text = "อีเมล์";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Location = new Point(445, 58);
-            label4.Name = "label4";
-            label4.Size = new Size(57, 20);
-            label4.TabIndex = 13;
-            label4.Text = "ตำเเหน่ง";
-            // 
-            // KeeperEditForm
+            // SpeciesInfoEditForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1328, 706);
-            Controls.Add(label4);
-            Controls.Add(label3);
-            Controls.Add(label2);
-            Controls.Add(label1);
-            Controls.Add(txtEmail);
-            Controls.Add(btnOK);
-            Controls.Add(btnEdit);
-            Controls.Add(btnDelete);
-            Controls.Add(btnAdd);
+            ClientSize = new Size(1287, 702);
             Controls.Add(dataGridView1);
-            Controls.Add(comboPosition);
-            Controls.Add(numericCapacity);
-            Controls.Add(txtPhone);
-            Controls.Add(txtName);
-            Name = "KeeperEditForm";
-            Text = "KeeperEditForm";
-            Load += KeeperEditForm_Load;
-            ((System.ComponentModel.ISupportInitialize)numericCapacity).EndInit();
+            Controls.Add(btnFeed);
+            Controls.Add(btnZoneAnimals);
+            Controls.Add(btnAllAnimals);
+            Name = "SpeciesInfoEditForm";
+            Text = "SpeciesInfoEditForm";
+            Load += SpeciesInfoEditForm_Load;
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
-        private Microsoft.Data.SqlClient.SqlCommand sqlCommand1;
-        private Microsoft.Data.SqlClient.SqlCommand sqlCommand2;
-        private TextBox txtName;
-        private TextBox txtPhone;
-        private NumericUpDown numericCapacity;
-        private ComboBox comboPosition;
+        private Button btnAllAnimals;
+        private Button btnZoneAnimals;
+        private Button btnFeed;
         private DataGridView dataGridView1;
-        private Button btnAdd;
-        private Button btnDelete;
-        private Button btnEdit;
-        private Button btnOK;
-        private TextBox txtEmail;
-        private Label label1;
-        private Label label2;
-        private Label label3;
-        private Label label4;
     }
 }
