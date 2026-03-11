0992b142cca052c2b4810d2172b9f5bafa1e9603
Inthira4548 <kineqwasg@gmail.com>
2026-03-11 16:31:33 +0700

    Update KeeperForm layout and controls

--- ZooManagement/KeeperForm.Designer.cs
+++ ZooManagement/KeeperForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace ZooManagement
+namespace ZooManagement
 {
     partial class KeeperForm
     {
@@ -28,12 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "KeeperForm";
+            dataGridView1 = new DataGridView();
+            btnLoad = new Button();
+            txtSearch = new TextBox();
+            btnSearch = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(95, 213);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(1106, 438);
+            dataGridView1.TabIndex = 0;
+            // 
+            // btnLoad
+            // 
+            btnLoad.Location = new Point(920, 112);
+            btnLoad.Name = "btnLoad";
+            btnLoad.Size = new Size(94, 29);
+            btnLoad.TabIndex = 1;
+            btnLoad.Text = "ค้นหา";
+            btnLoad.UseVisualStyleBackColor = true;
+            btnLoad.Click += btnLoad_Click;
+            // 
+            // txtSearch
+            // 
+            txtSearch.Location = new Point(126, 112);
+            txtSearch.Name = "txtSearch";
+            txtSearch.Size = new Size(576, 27);
+            txtSearch.TabIndex = 2;
+            // 
+            // btnSearch
+            // 
+            btnSearch.Location = new Point(762, 111);
+            btnSearch.Name = "btnSearch";
+            btnSearch.Size = new Size(121, 29);
+            btnSearch.TabIndex = 3;
+            btnSearch.Text = "ตกลง";
+            btnSearch.UseVisualStyleBackColor = true;
+            btnSearch.Click += btnSearch_Click;
+            // 
+            // KeeperForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1272, 675);
+            Controls.Add(btnSearch);
+            Controls.Add(txtSearch);
+            Controls.Add(btnLoad);
+            Controls.Add(dataGridView1);
+            Name = "KeeperForm";
+            Text = "KeeperForm";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private DataGridView dataGridView1;
+        private Button btnLoad;
+        private TextBox txtSearch;
+        private Button btnSearch;
     }
 }
