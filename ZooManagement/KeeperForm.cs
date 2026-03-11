db9e9ad45a05b70eea4e674bdcad2e2d2790d941
Inthira4548 <kineqwasg@gmail.com>
2026-03-11 16:30:48 +0700

    Implement load and search features for keepers
    Added functionality to load and search keepers from the database.

--- ZooManagement/KeeperForm.cs
+++ ZooManagement/KeeperForm.cs
@@ -1,10 +1,12 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
+using System.Data;
 
 namespace ZooManagement
 {
@@ -14,5 +16,35 @@ namespace ZooManagement
         {
             InitializeComponent();
         }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = connectDB.ConnectZooDB();
+
+            string sql = "SELECT * FROM keeper";
+
+            SqlDataAdapter da = new SqlDataAdapter(sql, conn);
+
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            dataGridView1.DataSource = dt;
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+			SqlConnection conn = connectDB.ConnectZooDB();
+
+			string sql = "SELECT * FROM keeper WHERE name LIKE @name";
+
+			SqlDataAdapter da = new SqlDataAdapter(sql, conn);
+
+			da.SelectCommand.Parameters.AddWithValue("@name", "%" + txtSearch.Text + "%");
+
+			DataTable dt = new DataTable();
+			da.Fill(dt);
+
+			dataGridView1.DataSource = dt;
+		}
     }
 }
